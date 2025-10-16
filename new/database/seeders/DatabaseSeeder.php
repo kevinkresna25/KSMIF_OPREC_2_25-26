6002028be40b007d6c299f906bd8c75fbe45482b
kevinkresna25 <kevinkresna25@gmail.com>
2025-10-16 19:24:42 +0700

    change pass

--- new/database/seeders/DatabaseSeeder.php
+++ new/database/seeders/DatabaseSeeder.php
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder
             [
                 'name' => 'KSMIF Operator',
                 'email' => 'operator@ksmif.test',
-                'password' => Hash::make('password'),
+                'password' => Hash::make('Password321'),
                 'is_operator' => true,
             ]
         );
